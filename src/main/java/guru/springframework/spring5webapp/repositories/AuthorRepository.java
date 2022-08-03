de865eed370d813d7d69f3da0bbbf7b5101b1a9c
Werstef <st.efrem98@gmail.com>
2022-08-03 12:58:11 +0300

    Initializing Data

--- src/main/java/guru/springframework/spring5webapp/repositories/AuthorRepository.java
+++ src/main/java/guru/springframework/spring5webapp/repositories/AuthorRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.repository.CrudRepository;
 import guru.springframework.spring5webapp.domain.Author;
 
 public interface AuthorRepository extends CrudRepository<Author, Long> {
-
 }
