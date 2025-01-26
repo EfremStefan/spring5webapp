fa1916c63037dca6a8c4622bb7564d5deb35e30e
Efrem Stefan <st.efrem98@gmail.com>
2025-01-26 21:44:07 +0200

    fix: annotate Service as component and update Book annotation

--- src/main/java/guru/springframework/spring5webapp/services/BookService.java
+++ src/main/java/guru/springframework/spring5webapp/services/BookService.java
@@ -2,11 +2,13 @@ package guru.springframework.spring5webapp.services;
 
 import guru.springframework.spring5webapp.models.Book;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+@Service
 public class BookService {
 
     private final BookRepository bookRepository;
