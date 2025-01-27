1088a5dd041f5d976eca7c3e3704fcfefc6b345e
sefrem <sefrem@c-s.ro>
2025-01-27 14:08:16 +0200

    fix: add Component annotation to Service

--- src/main/java/guru/springframework/spring5webapp/services/AuthorService.java
+++ src/main/java/guru/springframework/spring5webapp/services/AuthorService.java
@@ -2,11 +2,13 @@ package guru.springframework.spring5webapp.services;
 
 import guru.springframework.spring5webapp.models.Author;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+@Service
 public class AuthorService {
 
     private final AuthorRepository authorRepository;
